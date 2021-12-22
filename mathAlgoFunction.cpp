e48b777d2ea00c205ee02d345ff447d900b8c68f
Benjamin Kis <Kis@Pc_de_Benjamin>
2021-12-22 10:05:09 +0100

    first commit

---
+++ mathAlgoFunction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N,u,i=0,v,w;
+cout<<"Tapez la valeur de N : ";cin>>N;
+u=3;
+v=2;
+if(N==0)w=u;
+else if(N==1)w=v;
+else for(i=2;i<=N;i++){w=i*v+(i+1)*u+i;u=v;v=w;}
+cout<<"u("<<N<<")="<<w<<endl;
+return 0;
+}
